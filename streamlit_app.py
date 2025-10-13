1c11581a152a669be9161f3cd88f25d57d4cb70e
Zane Davis <zdavis11@elon.edu>
2025-10-13 18:51:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -972,7 +972,8 @@ for tab_name, tab in zip(tabs_to_show, tabs):
                 continue
 
         if tab_name == "Pitching":
-            df_filtered = df_filtered.drop(columns=[c for c in ["FIP", "SB%", "BA/RISP"] if c in d
+            df_filtered = df_filtered.drop(columns=[c for c in ["FIP", "SB%"] if c in df_filtered.columns])
+
 
         
         if df_filtered.empty:
